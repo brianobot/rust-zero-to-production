a2cf81e8b75245a0263a074a5ab5ac423034999b
Brian Obot <brianobot@gmail.com>
2026-02-11 15:39:54 +0100

    First Functional Web App

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpServer, Responder, get};
+
+#[get("/")]
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
